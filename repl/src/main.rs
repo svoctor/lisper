028aa0f9dc34121b360e7ea7eff83f312d8e39ed
Victor <me@victr.com>
2021-01-07 22:25:01 -0500

    Workspaces migration [WIP] (#1)
    * Creating cargo workspaces and moving files around
    * Added repl and wasm crates, with dependencies
    * First working REPL, loop listening for input
    * Print current version at start
    * Evaluate input and output evaluated result
    * Support for commands, such as /quit
    * Clean up and comments

---
+++ repl/src/main.rs
@@ -0,0 +1,55 @@
+use std::io;
+use std::io::Write;
+use lisper;
+
+// Get package version defined in cargo.toml
+const PKG_VERSION:&str = env!("CARGO_PKG_VERSION");
+
+
+fn evaluate(exp:String, env: &mut lisper::LisperEnv) -> Result<String, lisper::LisperErr> {
+    let tokens:Vec<String> = lisper::tokenize(exp);
+    let (parsed_tokens, _) = lisper::parse(&tokens)?;
+    let eval_out = lisper::eval(parsed_tokens, env)?;
+
+    Ok(eval_out.to_string())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    // Create lisper environment
+    let env = &mut lisper::create_default_env();
+    
+    // Welcome message, including current version
+    println!("Lisper v{}", PKG_VERSION);
+
+    loop {
+        // Input buffer
+        let mut input_buffer = String::new();
+
+        let stdin = io::stdin();
+
+        // Input prompt, including flush to force print
+        print!("$ ");
+        io::stdout().flush().expect("Unable to flush output");
+
+        // Read input string
+        stdin.read_line(&mut input_buffer).expect("Unable to read line");
+        let expr:String = input_buffer.trim().to_string();
+
+        // Match string to supported commands
+        match expr.as_str() {
+            "/quit" => {
+                // Exit the process
+                std::process::exit(0);
+            },
+            _ => {
+                // Evaluate the string as a lisper expression
+                match evaluate(expr, env) {
+                    Ok(res) => println!("{}", res),
+                    Err(e) => match e {
+                    lisper::LisperErr::Reason(msg) => println!("// 🙀 => {}", msg),
+                    },
+                }
+            }
+        }
+    }
+}
